f7aa224103557b1044428288f15c5d99262ba291
pankof <p.kofopoulos@gmail.com>
2018-09-07 14:03:38 +0300

    Commit 2

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -26,6 +26,7 @@ export class ProductListComponent implements OnInit {
     filteredProducts: IProduct[];
     products: IProduct[];
 
+    
     constructor(private productService: ProductService){
         
     }
